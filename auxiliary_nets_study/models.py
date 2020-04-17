12a2a662a357130be3bbfa3aba579fb0e7e82bbe
edouardoyallon <edouard.oyallon@ens.fr>
2020-04-17 20:04:05 +0200

    test

--- auxiliary_nets_study/models.py
+++ auxiliary_nets_study/models.py
@@ -270,12 +270,12 @@ class VGGn(nn.Module):
         feat_mult (float): Multiply number of feature maps with this number.
     '''
 
-    def __init__(self, cfg, input_dim=32, input_ch=3, num_classes=10,
+    def __init__(self, vggname, input_dim=32, input_ch=3, num_classes=10,
                  feat_mult=1, dropout=0.0, nonlin="relu", no_similarity_std=False,
                  loss_sup="predsim", dim_in_decoder=2048,
                  num_layers=0, num_hidden=1024):
         super(VGGn, self).__init__()
-        self.cfg = cfg
+        self.cfg = cfg[vggname]
         self.input_dim = input_dim
         self.input_ch = input_ch
         self.num_classes = num_classes
